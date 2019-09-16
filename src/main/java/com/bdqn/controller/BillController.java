3e4b1899078c20ea8ecd09677f00f881b7834ac9
xsy <2362247846@qq.com>
2019-09-16 14:55:35 +0800

    完成了订单控制器的编写

---
+++ src/main/java/com/bdqn/controller/BillController.java
@@ -0,0 +1,12 @@
+package com.bdqn.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * TODO 订单控制器
+ */
+@Controller
+@RequestMapping("/bill")
+public class BillController {
+}
